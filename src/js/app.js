22586c488164efdf8c8f1819ca52e4ffbb673827
Muhammed Yaşar Bardaklar <myasarbardaklar47@gmail.com>
2019-01-13 00:27:29 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,5 @@
 $(document).ready(function() {
 
-  var modalStatus = $('input[type="radio"].pxl-modal-trigger--on').prop('checked');
-  if(modalStatus) {
-    $('body').addClass('pxl-modal--on');
-  }
 
-  $('input[type="radio"].pxl-modal-trigger--on').change(function() {
-    var status = $(this).prop('checked');
-    
-    if(status) {
-      $('body').addClass('pxl-modal--on');
-    }
-  });
-  $('input[type="radio"].pxl-modal-trigger--off').change(function() {
-    var status = $(this).prop('checked');
-    
-    if(status) {
-      $('.pxl-modal').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
-        $('body').removeClass('pxl-modal--on');
-      })
-    }
-  });
-
+  
 });
