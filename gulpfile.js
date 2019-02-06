8ee22345a622f11be1402a823c5943d634d77325
Muhammed Yaşar Bardaklar <myasarbardaklar47@gmail.com>
2019-02-06 17:58:10 +0300

    gulp file changed

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,7 @@ const sass          = require('gulp-sass'),
 
 // JS related plugins
 const uglify        = require('gulp-uglify'),
-      babelify      = require('babelify'),
-      browserify    = require('browserify'),
-      source        = require('vinyl-source-stream'),
-      buffer        = require('vinyl-buffer'),
-      stripDebug    = require('gulp-strip-debug');
+      babel         = require('gulp-babel');
 
 // Image related plugins
 const imagemin      = require('gulp-imagemin');
@@ -24,9 +20,7 @@ const imagemin      = require('gulp-imagemin');
 // Utility plugins
 const rename        = require('gulp-rename'),
       sourcemaps    = require('gulp-sourcemaps'),
-      plumber       = require('gulp-plumber'),
-      options       = require('gulp-options'),
-      gulpif        = require('gulp-if');
+      plumber       = require('gulp-plumber');
 
 // Browers related plugins
 const browserSync   = require('browser-sync').create();
@@ -84,6 +78,17 @@ gulp.task('sass', () => {
 
 // Task for compiling JS files
 gulp.task('js', () => {
+  return gulp.src(jsSRC)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(jsURL))
+    .pipe(browserSync.stream());
+
   return browserify(jsSRC)
     .transform(babelify, { presets: ['@babel/preset-env'] })
     .bundle()
